0c987e1b17990a9cfa313e6648e73745c1e59ef3
Jason Meurer <jasonmeurer32@gmail.com>
2021-04-19 18:01:03 -0400

    update notes

--- test.bash
+++ test.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 until resp=$(curl -s -S -g --max-time 3 --insecure "http://neverssl.com");do
-if [[ $resp == *"[CDATA[fuck"* ]] ; then
+if [[ $resp == *"[CDATA[success"* ]] ; then
     echo "found"
     break
   fi
