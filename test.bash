4b2fdba31123c35578645fc1129846cf6c55071e
Ubuntu <ubuntu@ip-10-20-0-227.us-west-2.compute.internal>
2021-04-23 15:25:46 +0000

    working east-west-outbound

--- test.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-until resp=$(curl -s -S -g --max-time 3 --insecure "http://neverssl.com");do
-if [[ $resp == *"[CDATA[success"* ]] ; then
-    echo "found"
-    break
-  fi
-  echo "not found"
-  sleep 10s
-done  
